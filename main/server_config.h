e71780b587bc63d21f7acdf9e24ca53e5ab0987c
Paul Bock <bocklucas@yahoo.ca>
2021-05-20 14:03:48 -0500

    Removing MQTT broker

--- main/server_config.h
+++ main/server_config.h
@@ -1,7 +1,7 @@
 #define DHT_PIN D5
 #define DHT_TYPE DHT11
 #define LOOP_DELAY 5000
-#define MQTT_BROKER "192.168.1.210";
+#define MQTT_BROKER "";
 #define MQTT_PORT 1883
 #define SETUP_DELAY 500
 #define VERSION "0.0.0"
